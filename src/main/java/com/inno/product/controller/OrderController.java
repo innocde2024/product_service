89c06ac9d3a6ff6a987ac4d05f1bc820d50d1834
PC <muoivo.051064@gmail.com>
2024-07-12 04:03:53 +0700

    kiet update trees

--- src/main/java/com/inno/product/controller/OrderController.java
+++ src/main/java/com/inno/product/controller/OrderController.java
@@ -3,8 +3,10 @@ package com.inno.product.controller;
 import com.inno.product.entity.OrderDTO;
 import com.inno.product.entity.UserDTO;
 import com.inno.product.model.Order;
+import com.inno.product.model.OrderItem;
 import com.inno.product.service.auth.AuthService;
 import com.inno.product.service.order.OrderService;
+import com.inno.product.service.orderItem.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
     @Autowired
+    private OrderItemService orderItemService;
+    @Autowired
     private AuthService authService;
 
     @GetMapping("/orders")
@@ -28,6 +32,33 @@ public class OrderController {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(list);
+    }
+    @GetMapping("/trees")
+    public ResponseEntity<?> getAllTrees() {
+        double amount = 0.0;
+        List<Order> list = orderService.getOrderList();
+        if(list == null) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            for (Order order : list) {
+                amount+=orderItemService.calculatePrice(order.getOrderItems());
+            }
+            return ResponseEntity.ok(amount/50000);
+        }
+    }
+    @GetMapping("/trees/users")
+    public ResponseEntity<?> getAllTreesPersonal(@RequestHeader("Authorization") String token) {
+        double amount = 0.0;
+        UserDTO userDTO = authService.verifyToken(token);
+        List<Order> list = orderService.getOrderListPersonal(userDTO.getId());
+        if(list == null) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            for (Order order : list) {
+                amount+=orderItemService.calculatePrice(order.getOrderItems());
+            }
+            return ResponseEntity.ok(amount/50000);
+        }
     }
 
     @GetMapping("/orders/users")
