3e92004cc1a8a483a9e024d476ef94d442084b07
PC <muoivo.051064@gmail.com>
2024-07-12 01:50:07 +0700

    kiet update finish 1h49 12/7

--- src/main/java/com/inno/product/service/orderItem/OrderItemServiceImpl.java
+++ src/main/java/com/inno/product/service/orderItem/OrderItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.inno.product.service.orderItem;
 
+import com.inno.product.exception.ApiRequestException;
 import com.inno.product.model.Order;
 import com.inno.product.model.OrderItem;
 import com.inno.product.model.Product;
@@ -7,6 +8,7 @@ import com.inno.product.repository.OrderItemRepository;
 import com.inno.product.service.order.OrderService;
 import com.inno.product.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -65,12 +67,16 @@ public class OrderItemServiceImpl implements OrderItemService {
     @Override
     public OrderItem createOrderItemByProductIdAndQuantity(int productId, int quantity, Order order) {
         Product product = productService.findById(productId);
-        OrderItem orderItem = new OrderItem();
-        orderItem.setProduct(product);
-        orderItem.setQuantity(quantity);
-        orderItem.setOrder(order);
-        orderItem.setPrice(product.getPrice()*quantity);
-        orderItemRepository.save(orderItem);
-        return orderItem;
+        if(product.getQuantity() >= quantity) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(product);
+            orderItem.setQuantity(quantity);
+            orderItem.setOrder(order);
+            orderItem.setPrice(product.getPrice() * quantity);
+            orderItemRepository.save(orderItem);
+            product.setQuantity(product.getQuantity() - quantity);
+            productService.addProduct(product);
+            return orderItem;
+        } else throw new ApiRequestException("bad_request", HttpStatus.BAD_REQUEST);
     }
 }
